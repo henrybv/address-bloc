c7c709d27a26cdc626c0bf156d8e79c0f23a54c8
Henry Bao-Viet Nguyen <hbn@Henrys-MacBook-Pro.local>
2017-06-08 17:32:57 -0700

    checkpoint4

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,24 +1,25 @@
 require_relative '../models/address_book'
 
-RSpec.describe AddressBook do
-	describe "attributes" do
-		it "responds to entries" do
-			book = AddressBook.new
-			expect(book).to respond_to(:entries)
-		end
-	end
+ RSpec.describe AddressBook do
+   describe "attributes" do
+     it "responds to entries" do
+       book = AddressBook.new
+       expect(book).to respond_to(:entries)
+     end
 
-	it "initializes entries as an array" do
-		book = AddressBook.new
-		expect(book.entries).to be_an(Array)
-	end
+     it "initializes entries as an array" do
+       book = AddressBook.new
+       expect(book.entries).to be_an(Array)
+     end
 
-	it "initializes entries as empty" do
-		book = AddressBook.new
-		expect(book.entries.size).to eq(0)
-	end
+     it "initializes entries as empty" do
+       book = AddressBook.new
+       expect(book.entries.size).to eq(0)
+      end
+    end
 
-	describe "#add_entry" do
+ 
+   describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -36,6 +37,22 @@ RSpec.describe AddressBook do
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
+ end
 
+# describe "#remove_entry" do
+# 	it "removes an entry by name, phone_number, and email address" do
+# 		book = AddressBook.new
+# 		book.add_entry('Bill Gates', '650.888.9900', 'bill@microsoft.com')
 
-end
+# 		name = "Ada Lovelace"
+# 		phone_number = "010.012.1815"
+# 		email_address = 'augusta.king@lovelace.com'
+# 		book.add_entry(name, phone_number, email_address)
+
+# 		expect(book.entries.size).to eq 2
+# 		book.remove_entry(name, phone_number, email_address)
+# 		expect(book.entries.size).to eq 1
+# 		expect(book.entries.first.name).to eq("Bill Gates")
+# 	end
+# end
+
